01a14b01ff36574ade0ac7ee7691ad1acbeacc2d
Victor Kozlov <punkrus18@gmail.com>
2017-02-07 10:43:06 +0300

    split input values; padding to input-container;

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@
 let app = angular.module('testDynamicInput', ['saprun-dynamic-input']);
 
 app.controller('testCtrl', ['$scope', function($scope) {
-    $scope.inputValue = 0;
+    $scope.first = 0;
+    $scope.second = 0;
+    $scope.third = 0;
     $scope.changeHandler = () => {
         console.log("U have change me!");
     }
