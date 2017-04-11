537a998bb88c58e45fb654ceb0880bef0f26cf0c
Victor Kozlov <punkrus18@gmail.com>
2017-04-11 17:02:21 +0300

    add required;

--- src/saprun-dynamic-input.js
+++ src/saprun-dynamic-input.js
@@ -8,6 +8,8 @@ saprunDynamicInput.directive('saprunDynamicInput', function() {
                 <div class="container" tabindex="1">
                     <div class="input-container">
                       <input id="{{inputid}}" ng-model="value" ng-change="changeHandler()" maxlength="{{maxlength}}">
+                      <input class="dynamic-input_required" required ng-model="requiredValue">
+
                       <div class="spacer" ng-bind="charSpacer + value"></div>
                     </div>
                     <span class="unit" ng-bind="unit"></span>
@@ -18,15 +20,19 @@ saprunDynamicInput.directive('saprunDynamicInput', function() {
                 change: "&",
                 unit: "@",
                 maxlength: "=?",
-                inputid: "@"
+                inputid: "@",
+                required: "@"
             },
             link: function(scope) {
                 if( scope.unit == null ) { scope.unit = ""; }
                 if( scope.maxlength == null ) { scope.maxlength = ""; }
                 if( scope.inputid == null) { scope.inputid = `saprunDynamicInput-${scope.$id}`; }
+                scope.$watch("value", () => {
+                    scope.requiredValue = ( ( scope.value != null ) && scope.value.toString().length ) ||
+                        ( scope.required != "true" ) ?  "1" : "";
+                });
                 scope.changeHandler = () => {
-                    let currentValue = scope.value;
-                    scope.charSpacer = ( currentValue.length == 0 ) ? "0" : "";
+                    scope.charSpacer = ( ( scope.value != null ) && scope.value.toString().length == 0 ) ? "0" : "";
                     setTimeout(scope.change, 0);
                 }
             }
